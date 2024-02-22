d1318dcb9a2996f8b2e3b56e039c717eed051e2d
AMAN-22byte <gettoknowaman@gmail.com>
2024-02-22 10:40:50 +0530

    aman1

---
+++ entry/security.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class login{
+    private:
+    string firstname;
+    string lastname;
+    int age;
+    public:
+    string username;
+    
+}
+
+int main(){
+    return 0;
+}
